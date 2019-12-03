df1871a35eb7aa23e5f52639275bb7336fe816a5
Esteve Segura <estevesegura.gir@gmail.com>
2019-12-03 23:20:38 +0100

    react front

---
+++ dockify/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Index from './Pages/Index';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/"> 
+          <Index />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
